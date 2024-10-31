b8b526ba3f29d105f5482c38edbdb8d4a9fbf430
AlinkaKuznets <linaolen6@mail.ru>
2024-10-31 19:24:08 +0700

    Added cart view, footer, map, 'About Us' section

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,53 @@
 window.addEventListener('scroll', function () {
     document.getElementById('header-nav').classList.toggle('headernav-scroll', window.scrollY > 132);
 })
+
+const offcanvasCartEl = document.getElementById('offcanvasCart');
+const offcanvasCart = new bootstrap.Offcanvas(offcanvasCartEl);
+
+document.getElementById('cart-open').addEventListener('click', (e) =>{
+    e.preventDefault();
+    offcanvasCart.toggle()
+});
+
+document.querySelectorAll('.closecart').forEach(item => {
+    item.addEventListener('click', e => {
+        e.preventDefault();
+        offcanvasCart.hide();
+        let href = item.href.split('#').pop();
+        document.getElementById(href).scrollIntoView();
+    });
+});
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('#top').fadeIn();
+        } else {
+            $('#top').fadeOut();
+        }
+    });
+
+    $('#top').click(() => {
+        $('html,body').animate({ scrollTop: 0 }, 300);
+        return false;
+    });
+
+    $(".owl-carousel-full").owlCarousel({
+        margin: 20,
+        responsive: {
+            0: {
+                items: 1
+            },
+            500: {
+                items: 2
+            },
+            730: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+});
